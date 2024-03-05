bd430dc8fac85c4100d170440a2164cb3e6590f0
William <william.bjorklund.00@gmail.com>
2024-03-06 00:24:44 +0100

    Sign up form finished!

--- WebApp2/Models/SignUpFormModel.cs
+++ WebApp2/Models/SignUpFormModel.cs
@@ -9,24 +9,27 @@ namespace AspNetCore.Models
         [Required(ErrorMessage = "Invalid first name")]
         public string FirstName { get; set; } = null!;
 
-        [DisplayName("Last name")]
+        [Display(Name = "Last name", Prompt = "Enter your last name")]
         [Required(ErrorMessage = "Invalid last name")]
         public string LastName { get; set; } = null!;
 
-        [DisplayName("Email address")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address", Prompt = "Enter your email address")]
         [Required(ErrorMessage = "Invalid email address")]
         public string Email { get; set; } = null!;
 
-        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password", Prompt = "Enter your password")]
         [Required(ErrorMessage = "Invalid password")]
         public string Password { get; set; } = null!;
 
-        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password", Prompt = "Confirm your password")]
         [Required(ErrorMessage = "Must confirm password")]
         [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; } = null!;
 
-        [DisplayName("Terms & Conditions")]
+        [Display(Name = "I agree to the Terms & Conditions")]
         [Required(ErrorMessage = "You must agree to the terms & conditions")]
         public bool Terms { get; set; }
     }
